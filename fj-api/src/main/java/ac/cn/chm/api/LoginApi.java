dd2234172a9c08d289b316ff59a18dcad74a3097
chm <chm@chm.cn>
2018-08-26 17:50:04 +0800

    添加一个api文件，用于后台跨系统获取数据

---
+++ fj-api/src/main/java/ac/cn/chm/api/LoginApi.java
@@ -0,0 +1,9 @@
+package ac.cn.chm.api;
+/**
+ * 登录注销的api
+ * @author chm
+ * @data 2018-08-26
+ */
+public class LoginApi {
+
+}
---
+++ fj-api/src/main/java/ac/cn/chm/api/UserApi.java
@@ -0,0 +1,10 @@
+package ac.cn.chm.api;
+/**
+ * 操作用户信息的的api
+ * @author chm
+ * @data 2018-08-26ss
+ *
+ */
+public class UserApi {
+
+}
